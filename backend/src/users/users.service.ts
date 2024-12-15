92c15d5763f474b4e381091592adae618e0007cc
auermatew <auermate2005@gmail.com>
2024-12-16 00:21:05 +0100

    +add prisma service

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
+  constructor(private readonly prisma: PrismaService) {}
+
+/*   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
-
-  findAll() {
-    return `This action returns all users`;
+ */
+  async findAll() {
+    return await this.prisma.user.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: number) {
+    return await this.prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
