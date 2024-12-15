d7d7af8c221eebd075005357868af4a36ef5f967
bolond <sadfoolc@gmail.com>
2024-12-15 20:04:17 +0100

    magozás

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -3,17 +3,22 @@ import { faker } from '@faker-js/faker';
 const prisma = new PrismaClient()
 
 const productTypesArray = ["animal","toy","good product","bad product"]
-enum productTypes {"animal","toy","good product","bad product"}
+// enum productTypes {"animal","toy","good product","bad product"}
 
 async function main() {
     for (let n = 0; n < productTypesArray.length; n++) {
-        await prisma.productType.create({
-            data: {
-                productType: productTypesArray[n]
-            }
-        })
+        const existingType = await prisma.productType.findUnique({
+            where: { productType: productTypesArray[n] }
+        });
+
+        if (!existingType) {
+            await prisma.productType.create({
+                data: { productType: productTypesArray[n] }
+            });
+        }
     }
-  for (let i = 0; i < 1000; i++) {
+
+  for (let i = 1; i < 100; i++) {
     await prisma.user.create({
         data: {
             email: faker.internet.email(),
@@ -21,7 +26,24 @@ async function main() {
             password: faker.internet.password()
         }
     })
-    
+    await prisma.product.create({
+        data:{
+            name: faker.lorem.word(),
+            description: faker.lorem.words(10),
+            price: faker.number.int({min:500,max:50000,multipleOf:500}),
+            image: faker.image.urlLoremFlickr(),
+            type: productTypesArray[faker.number.int({min:0,max:productTypesArray.length-1})]
+        }
+    })
+    await prisma.cart.create({
+        data: {
+            user_id:i,
+            product_id:i,
+            amount:faker.number.int({min:1,max:50})
+        }
+    })
+
+
   }
 }
 main()
